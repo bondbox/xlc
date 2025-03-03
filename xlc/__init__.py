064471262573b8871a126d4d6227a3cb8aab2376
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-03 12:49:37 +0800

    database: export language-related classes

--- xlc/__init__.py
+++ xlc/__init__.py
@@ -1,1 +1,11 @@
 # coding:utf-8
+
+from .database import DATABASE  # noqa:F401
+from .database import LANGUAGES  # noqa:F401
+from .database import LangItem  # noqa:F401,H306
+from .database import LangT  # noqa:F401
+from .database import LangTag  # noqa:F401
+from .database import LangTags  # noqa:F401
+from .database import Language  # noqa:F401
+from .database import Region  # noqa:F401
+from .database import Script  # noqa:F401
--- xlc/database/__init__.py
+++ xlc/database/__init__.py
@@ -5,6 +5,9 @@ from .langtags import LangItem  # noqa:F401,H306
 from .langtags import LangT  # noqa:F401
 from .langtags import LangTag  # noqa:F401
 from .langtags import LangTags
+from .subtags import Language  # noqa:F401
+from .subtags import Region  # noqa:F401
+from .subtags import Script  # noqa:F401
 
 
 class Database:
