c84e1b21d8fcf6caae3dd89a60dd59f4e48aeda9
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-12 16:30:08 +0800

    xlc-generate: add output directory option

--- xlc-tools/xlc_tools/generate.py
+++ xlc-tools/xlc_tools/generate.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 
+import os
 from typing import List
 from typing import Optional
 from typing import Sequence
@@ -11,26 +12,26 @@ from xkits import run_command
 from xlc_tools.attribute import __urlhome__
 from xlc_tools.attribute import __version__
 
-from xlc import LangT
 from xlc import Segment
-
-
-def generate(langtag: LangT):
-    segment: Segment = Segment.generate(langtag)
-    segment.dumpf(f"{segment.lang.tag}.xlc")
 
 
 @add_command("xlc-generate", description="Generate xlc files.")
 def add_cmd(_arg: argp):
+    _arg.add_argument("--base", dest="directory", type=str, help="directory",
+                      metavar="DIR", default="locale")
     _arg.add_argument(dest="languages", type=str, help="language", nargs="*",
                       metavar="LANG", default=["en", "zh-Hans", "zh-Hant"])
 
 
 @run_command(add_cmd)
 def run_cmd(cmds: commands) -> int:
+    directory: str = cmds.args.directory
     languages: List[str] = cmds.args.languages
+    os.makedirs(directory, exist_ok=True)
     for language in languages:
-        generate(language)
+        segment: Segment = Segment.generate(language)
+        filename: str = f"{segment.lang.tag.name}.xlc"
+        segment.dumpf(os.path.join(directory, filename))
     return 0
 
 
