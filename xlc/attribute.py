6d2e39619ddff19169da4cf856bc51535ef47b05
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-13 11:40:49 +0800

    bump version to 0.3.0

--- xlc/attribute.py
+++ xlc/attribute.py
@@ -3,7 +3,7 @@
 from urllib.parse import urljoin
 
 __project__ = "xlc"
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __description__ = "Internationalization(i18n) and Localization(l10n) of Multiple Languages"  # noqa:E501
 __urlhome__ = "https://github.com/bondbox/xlc/"
 __urlcode__ = __urlhome__
