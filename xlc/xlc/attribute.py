37e618f0d4ffe4204c1625ebbfa4b639abfedc82
Mingzhe Zou <zoumingzhe@qq.com>
2026-06-17 20:39:51 +0800

    build: migrate from setup.py to pyproject.toml

---
+++ xlc/xlc/attribute.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+# project info
+__project_name__ = "xlc"
+__project_home__ = "https://github.com/bondbox/xlc/"
+__project_desc__ = "Internationalization(i18n) and Localization(l10n) of Multiple Languages"
+
+# package info
+__version__ = "1.1"
+__package_name__ = "xlc"
+__package_desc__ = "Internationalization(i18n) and Localization(l10n) of Multiple Languages"
